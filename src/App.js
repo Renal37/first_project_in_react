8772278a939d42901eab73dee37a2d08303b4967
Renal37 <renalplaycsgo@gmail.com>
2024-02-26 14:42:00 +0300

    create change name

--- src/App.js
+++ src/App.js
@@ -3,26 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render(){
+  constructor(){
+super();
+
+this.state = {
+  name:'Renal'
+}
+  };
+  render (){
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi {this.state.name}  
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <button 
+          onClick={()=>{
+            this.setState({name:'Dima'});
+          }}
           >
-            Learn React
-          </a>
+            Change name</button>
         </header>
       </div>
     );
   }
+
 }
 
 export default App;
