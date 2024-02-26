396026781d1edc890a53276d5c3fb970313bcff4
Renal37 <renalplaycsgo@gmail.com>
2024-02-26 17:02:44 +0300

    optimizations code

--- src/App.js
+++ src/App.js
@@ -23,20 +23,24 @@ class App extends Component {
         }
       ))
   }
+
+  onSearchChange =(event) => {
+    const searchfield =event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchfield };
+    });
+  }
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchfield);
+    const {monsters,searchfield}= this.state;
+    const {onSearchChange}=this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchfield);
     });
 
     console.log('render');
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
-          const searchfield =event.target.value.toLocaleLowerCase();
-          this.setState(() => {
-            return { searchfield };
-          })
-        }} />
+        <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange} />
         {
           filteredMonsters.map((monster) => {
             return <div key={monster.id}><h1>{monster.name}</h1></div>
