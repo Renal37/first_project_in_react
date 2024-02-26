15573603cd4c5ed6cb4acef9873fcc48de85c30b
Renal37 <renalplaycsgo@gmail.com>
2024-02-26 16:52:23 +0300

    upgrade input search

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchfield : ''
     };
     console.log('constructor');
   }
@@ -23,21 +24,21 @@ class App extends Component {
       ))
   }
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchfield);
+    });
+
     console.log('render');
     return (
       <div className="App">
         <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
-          const searchstr =event.target.value.toLocaleLowerCase();
-
-          const filteredMonsters = this.state.monsters.filter((monster) => {
-            return monster.name.toLocaleLowerCase().includes(searchstr);
-          });
+          const searchfield =event.target.value.toLocaleLowerCase();
           this.setState(() => {
-            return { monsters: filteredMonsters };
+            return { searchfield };
           })
         }} />
         {
-          this.state.monsters.map((monster) => {
+          filteredMonsters.map((monster) => {
             return <div key={monster.id}><h1>{monster.name}</h1></div>
           })
         }
