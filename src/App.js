338c2ac82375becbb5a146e4cf2f8c36958e64b5
Renal37 <renalplaycsgo@gmail.com>
2024-02-26 17:32:37 +0300

    add card for site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -10,10 +10,8 @@ class App extends Component {
       monsters: [],
       searchfield : ''
     };
-    console.log('constructor');
   }
   componentDidMount() {
-    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users) => this.setState(() => {
@@ -36,16 +34,10 @@ class App extends Component {
     const filteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchfield);
     });
-
-    console.log('render');
     return (
       <div className="App">
         <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange} />
-        {
-          filteredMonsters.map((monster) => {
-            return <div key={monster.id}><h1>{monster.name}</h1></div>
-          })
-        }
+       <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
