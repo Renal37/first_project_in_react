d0ccded68283fe4fc8ecab7c739515fc71a275a0
Renal37 <renalplaycsgo@gmail.com>
2024-02-26 15:33:11 +0300

    Mapping Array

--- src/App.js
+++ src/App.js
@@ -7,31 +7,30 @@ class App extends Component {
 super();
 
 this.state = {
-  name:{firstName:'Renal',lastName:'Mustafin'},
-  school: 'APT'
+  monsters:[ {
+    name:'Linda',
+    id:'12o31n2312'
+  },
+ {
+    name:'Frank',
+    id :'i1j23lasd'
+  },{
+    name:'Jack',
+    id:'1odksahd'
+  },{
+    name:'Ali',
+    id:'iasdj1'
+  }]
 }
   };
   render (){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I study at {this.state.school}  
-          </p>
-          <button 
-          onClick={()=>{
-            this.setState((state,props)=>{
-              return{
-                name:{firstName:'Dima',lastName:'Dima'}
-              }
-            },()=>{
-              console.log(this.state)
-            });
-          }}
-          >
-            Change name</button>
-        </header>
+        {
+          this.state.monsters.map((monster)=>{
+            return <div key={monster.id}><h1>{monster.name}</h1></div>
+          })
+        }
       </div>
     );
   }
